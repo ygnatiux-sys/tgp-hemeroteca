92a8eb7181d4ec7fe5430b8d289cf335812971fd
ygnatiux.sys <ygnatiux@gmail.com>
2026-09-22 07:16:37 -0300

    TGP Sync: Actualización unificada de arquitectura y contenido

--- tgp-mind/index.ts
+++ tgp-mind/index.ts
@@ -40,9 +40,9 @@ import {
   setHITLState,
   clearHITLState,
   registrarResguardoD1,
-} from './src/storage/d1.js';
+} from './src/storage/d1.ts';
 // -- Vision: Wikimedia Anti-Drift (ver src/vision/wikimedia.ts) ------------------
-import { procesarImagen, resolverEntidadCanonica, buscarPageImageWikipedia } from './src/vision/wikimedia.js';
+import { procesarImagen, resolverEntidadCanonica, buscarPageImageWikipedia } from './src/vision/wikimedia.ts';
 // -- Servicios: Publicacion GitOps (ver src/servicios/publicacion.ts) -------------
 import {
   initPublicacion,
